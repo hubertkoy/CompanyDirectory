d0b30bf7812917efff5db96de96f0b10582e7faa
hubertkoy <hubkoydev@gmail.com>
2021-05-26 07:48:36 +0100

    Merge seperate files per page to one

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,13 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import '../styles/app.scss';
+import 'bootstrap';
+import bsCustomFileInput from "bs-custom-file-input";
 
 // start the Stimulus application
-import './bootstrap';
+bsCustomFileInput.init();
+
+import {createApp} from "vue";
+import App from './pages/index';
+createApp(App).mount('#app');
