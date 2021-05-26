090f0cf94e8c8d43bf68793a915a574875eb0f87
hubertkoy <hubkoydev@gmail.com>
2021-05-26 18:33:47 +0100

    add user login link if not logged

--- assets/js/components/navbar.vue
+++ assets/js/components/navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark" aria-label="Eighth navbar example">
     <div class="container">
-      <a class="navbar-brand" href="#">Container</a>
+      <a class="navbar-brand" href="#">Company Directory</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -10,6 +10,9 @@
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item" v-for="page in pages">
             <a href="#" class="nav-link" @click="loadPage(page[0])" :class="{active: currentPage === page[0].toLowerCase()}"><i :class="page[1]"></i> {{ page[0] }}</a>
+          </li>
+          <li class="nav-item" v-if="!userLogged">
+            <a href="/login" class="nav-link">Login</a>
           </li>
         </ul>
       </div>
@@ -22,6 +25,7 @@ export default {
   name: "navbar",
   data: () => ({
     pages: [['Departments', 'fa fa-sitemap'], ['Locations', 'fa fa-globe'], ['Personnel', 'fa fa-address-card']],
+    userLogged: null,
   }),
   props: {
     currentPage: String,
@@ -30,6 +34,9 @@ export default {
     loadPage(value) {
       this.$emit('showPage', value.toLowerCase());
     }
+  },
+  created() {
+    this.userLogged = window.document.userLogged;
   }
 }
 </script>
